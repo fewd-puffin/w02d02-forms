9151a0cda8fb00d501ea446ea2c53f398d84f079
Jackie Casper <casper.jaclyn@gmail.com>
2019-04-15 16:34:07 +0300

    solutions

--- assignments/assignment2/main.js
+++ assignments/assignment2/main.js
@@ -1,5 +1,11 @@
 $(document).ready(function(){
   // when the #name-form is submitted, get the #name value and greet them in the h1!
-  
+  $('#name-form').on('submit', changeHeader);
+
+  function changeHeader(event) {
+    event.preventDefault();
+    var name = $('#name').val();
+    $('h1').text('Hello ' + name + '!');
+  }
 
 })
--- assignments/assignment3/main.js
+++ assignments/assignment3/main.js
@@ -11,19 +11,51 @@ $(document).ready(function(){
     // get the id of this input and assign it to a variable
     // if the id is red change the red variable to the value, if green change green, if blue, change blue
     // change the background color of the body using the updated red, green and blue values
-  
+    var value = $(this).val();
+    var id = $(this).attr('id');
+
+    if (id === 'red') {
+      red = value;
+    } else if (id === 'green') {
+      green = value;
+    } else {
+      blue = value;
+    }
+    $('body').css('background-color', 'rgb(' + red + ',' + green + ',' + blue + ')')
   }
+
+
+  // function changeBackground(event) {
+  //   event.preventDefault();
+  //   red = $('#red').val();
+  //   green = $('#green').val();
+  //   blue = $('#blue').val();
+  //   $('body').css('background-color', 'rgb(' + red + ', ' + green + ', ' + blue +') ');
+  // }
 
 
   $('#color-form').on('submit', saveColor)
 
   function saveColor(event){
     // prevent the default event action
+    event.preventDefault()
     // create a new div with a background color using the red green and blue values
+    var newDiv = $('<div>').css('background-color', 'rgb(' + red + ',' + green + ',' + blue + ')')
     // append it to #saved-colors
+    $('#saved-colors').append(newDiv);
   }
 
   // BONUS
   // have a random color when the page loads
+  var randomred = Math.ceil(Math.random() * 255);
+  var randomgreen = Math.ceil(Math.random() * 255);
+  var randomblue = Math.ceil(Math.random() * 255);
+  $('body').css('background - color', 'rgb('+randomred +', '+randomgreen +', '+randomblue +') ');
+  $('#red').val(randomred);
+  $('#green').val(randomgreen);
+  $('#blue').val(randomblue);
+  red = randomred;
+  green = randomgreen;
+  blue = randomblue;
 
 })
